e2363a148ab05a1da2ba9fbdb4bc409d841ca12f
JimmyYZHU <jyuzhu@umich.edu>
2023-12-02 17:07:59 -0500

    add GAC

--- ply_load.py
+++ ply_load.py
@@ -1,8 +1,12 @@
 from scene import GaussianModel
+from GAC.GACNet import GACNet
+
 
 
 def test_feature(gaussians, path):
     gaussians.load_ply(path)
+    num_class = 4
+    GAC_model = GACNet(num_class)
     
     a = 1
     
