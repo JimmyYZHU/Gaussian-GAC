Fixed backpropagation graph being called even after cache has been emptied
- Assignment operator and addition of loss and directly calling backward() from total loss created wrong behaviour

- Fixed with a some minimal changes (since code is temporary)

scene/GAC_features.py
@@ -9,16 +9,16 @@ import argparse
 class PlyLoader():
     def __init__(self, ply_path):
         self.max_sh_degree = 3
-        
+
         self.xyz = torch.empty(0)
         self.features_dc = torch.empty(0)
         self.features_rest = torch.empty(0)
         self.scaling = torch.empty(0)
         self.rotation = torch.empty(0)
         self.opacity = torch.empty(0)
-        
+
         self.load_ply(ply_path)
-        
+
     def load_ply(self, ply_path):
         plydata = PlyData.read(ply_path)
 
@@ -62,12 +62,12 @@ class PlyLoader():
 
     def get_full_data(self):
         # ouput: xyz: [B, C, N], feat: [B, D, N], C=3, D56
-        
+
         N, _ = self.xyz.shape
         # N = 4096
-        
+
         xyz = self.xyz.permute(1, 0)
-        
+
         # N*1*3, C=3
         feat_dc = self.features_dc.permute(1, 2, 0)
         feat_dc = feat_dc.squeeze()
@@ -87,16 +87,16 @@ class PlyLoader():
 
 
 def training():
-    ply_path = 'output/dd261d92-e/point_cloud/iteration_30000/point_cloud.ply'
+    ply_path = 'output/tandt/point_cloud/iteration_3000/point_cloud.ply'
     dataloader = PlyLoader(ply_path)
-    
+
     model = GACNet()
     model.to('cuda')
-    
+
     data_size = 4096
     # xyz_all: 3*N
     xyz_all, feat_all = dataloader.get_full_data()
-    
+
     # random shuffle
     shuffle_idx = torch.randperm(xyz_all.shape[1])
     sel_idxes = [shuffle_idx[(i*data_size):((i+1)*data_size)] for i in range(xyz_all.shape[1] // data_size)]
@@ -105,9 +105,7 @@ def training():
         last_block = xyz_all.shape[1] // data_size
         complemented_dat = torch.cat((shuffle_idx[(last_block*data_size):], shuffle_idx[:residual]))
         sel_idxes.append(complemented_dat)
-    
-    seg_feat = torch.zeros((1, xyz_all.shape[1], 128), device=xyz_all.device)
-    
+
     optimizer = torch.optim.Adam(
             model.parameters(),
             lr=0.01,
@@ -115,22 +113,29 @@ def training():
             eps=1e-08,
             weight_decay=1e-4
         )
-    
+
     num_epoch = 10
     criterion = nn.CrossEntropyLoss()
-    
-    for epoch in trange(num_epoch):
+
+    pbar = trange(num_epoch)
+    for epoch in pbar:
+        total_loss = 0
+
         model.train()
         optimizer.zero_grad()
+        seg_feat = torch.zeros((1, xyz_all.shape[1], 128), device=xyz_all.device)
         for idx in sel_idxes:
+            # B*N*D
+            # batch_pred = model(xyz_all[:, idx].unsqueeze(0), feat_all[:, idx].unsqueeze(0))
+            # loss = criterion(batch_pred, torch.ones(batch_pred.shape, device=xyz_all.device))
             seg_feat[:, idx, :] = model(xyz_all[:, idx].unsqueeze(0), feat_all[:, idx].unsqueeze(0))
-        
+
         loss = criterion(seg_feat, torch.ones(seg_feat.shape, device=seg_feat.device))
-        # loss.backward()
-        print(f"epoch {epoch} loss: {loss}")
+        loss.backward()
+        total_loss += loss.item()
+        pbar.set_description(f"epoch {epoch} total loss: {total_loss}")
         optimizer.step()
-    
-    print(seg_feat[0, :3, :10])
+
 
 
 